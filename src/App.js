252b66e17ca17feae7061069d5ec858294b478b4
Bruno Klein <bruno.klein@digitaldesk.com.br>
2017-12-15 11:11:10 -0200

    add coommon components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { CreateStore } from 'redux';
 import reducers from './reducers';
+import { header } from './components/common';
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View />
+      <View>
+        <Header headerText={"Tech Stack"} />
+      </View>
     </Provider>
   );
 };
